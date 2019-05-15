44630e41d2ad5039a9fb30f5db7d10d0d5f87904
Rocky Wu <rocky@17.media>
2019-05-15 18:17:56 +0800

    WIP

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,4 +1,8 @@
 import { ApolloServer, gql } from 'apollo-server'
+import got from 'got'
+import cheerio from 'cheerio'
+
+import xml from 'xml'
 
 const server = new ApolloServer({
   typeDefs: gql`
@@ -7,14 +11,35 @@ const server = new ApolloServer({
       author: String
     }
 
+    type Pet {
+      id: ID!
+      age: Int
+      sex: String
+    }
+
     type Query {
       books: [Book]
       hello: String
+      pets: [Pet]
     }
   `,
 
   resolvers: {
     Query: {
+      pets: async () => {
+        const res = await got(
+          'https://asms.coa.gov.tw/Asms/api/ViewNowAnimal?pageSize=200&currentPage=1&sortDirection=DESC&sortFields=AcceptDate',
+        )
+
+        const $ = cheerio.load(res.body, {
+          normalizeWhitespace: true,
+          xmlMode: true,
+        })
+
+        $.xml()
+
+        debugger
+      },
       hello: () => 'Hello Server Apollo 333333!!!!!!!!!',
       books: () => [
         {
